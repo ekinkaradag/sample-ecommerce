3dded62c5d53059f48c46ed216f5b2cc6bb82aa4
Ekin Karadag <6624768+ekinkaradag@users.noreply.github.com>
2020-11-14 13:27:12 +0100

    Updated styles

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,35 +11,35 @@ class Directory extends React.Component {
             sections: [
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: process.env.PUBLIC_URL + '/images/hats.jpg',
                     id: 1,
                     linkUrl: 'shop/hats'
                 },
                 {
                     title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imageUrl: process.env.PUBLIC_URL + '/images/jackets.jpg',
                     id: 2,
                     linkUrl: 'shop/jackets'
                 },
                 {
                     title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imageUrl: process.env.PUBLIC_URL + '/images/sneakers.jpg',
                     id: 3,
                     linkUrl: 'shop/sneakers'
                 },
                 {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    title: 'women',
+                    imageUrl: process.env.PUBLIC_URL + '/images/women.jpg',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
+                    linkUrl: 'shop/women'
                 },
                 {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    title: 'men',
+                    imageUrl: process.env.PUBLIC_URL + '/images/men.jpg',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
+                    linkUrl: 'shop/men'
                 }
             ]
         }
