c231242314428d0e5130525016b82081d612c567
Ekin Karadag <6624768+ekinkaradag@users.noreply.github.com>
2020-11-18 19:51:18 +0100

    Added a CustomButton component
    Added a CustomButton component and imported it to the "Sign In" page.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 
 import './sign-in.styles.scss';
 
@@ -46,8 +47,8 @@ class SignIn extends React.Component{
                             value={this.state.password}
                             handleChange={this.handleChange}
                             required />
-                            
-                    <input type='submit' value='Sign In' />
+
+                    <CustomButton type='submit'> SIGN IN</CustomButton>
                 </form>
             </div>
         )
