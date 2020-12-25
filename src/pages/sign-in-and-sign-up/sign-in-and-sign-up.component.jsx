10e910e1b0b459d249ec222b8ae90e44c07759fd
Ekin Karadag <6624768+ekinkaradag@users.noreply.github.com>
2020-12-25 01:03:14 +0100

    Added SignUp component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 import CustomButton from '../../components/custom-button/custom-button.component'
 import { auth } from '../../firebase/firebase.utils';
 
 import './sign-in-and-sign-up.styles.scss';
 
-const SignInAndSignUpPage = ({currentUser}) => (
+const SignInAndSignUpPage = ({ currentUser }) => (
     <div className='sign-in-and-sign-up'>
         {
-            (currentUser!==null) ?
-            <div className='signed-in-user'>
-                {
-                    (currentUser.photoURL!==null && currentUser.photoURL.includes('google')) ?
-                    <img src={currentUser.photoURL} />
-                    :
-                    <div></div>
-                }
-                <p>Hello {currentUser.displayName}</p>
-                <p>You are logged in with "{currentUser.email}"</p>
-                <CustomButton onClick={() => auth.signOut()}>SIGN OUT</CustomButton>
-            </div>
-            
-            :
-            <SignIn />
+            (currentUser !== null) ?
+                <div className='page'>
+                    {
+                        (currentUser.photoURL !== null && currentUser.photoURL.includes('google')) ?
+                            <img src={currentUser.photoURL} />
+                            :
+                            <div></div>
+                    }
+                    <p>Hello {currentUser.displayName}</p>
+                    <p>You are logged in with "{currentUser.email}"</p>
+                    <CustomButton onClick={() => auth.signOut()}>SIGN OUT</CustomButton>
+                </div>
+
+                :<div>
+                    <SignIn className='sign-in'></SignIn>
+                    <SignUp className='sign-up'></SignUp>
+                </div>
         }
     </div>
 );
